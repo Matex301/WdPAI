7715f2d21c46dafef6ba67ce6a7f23397f64eca6
Mateusz Sarnacki <mateks301@interia.pl>
2023-10-16 20:30:22 +0200

    Added test views

---
+++ public/js/tab-main.js
@@ -0,0 +1,14 @@
+function openTab(name) {
+    let nav = document.getElementsByClassName("nav-button");
+    for( i = 0; i < nav.length; i++){
+        nav[i].className = nav[i].className.replace(" active", "");
+    }
+
+    let tab = document.getElementsByClassName("tab");
+    for( i = 0; i < tab.length; i++){
+        tab[i].className = tab[i].className.replace(" active", "");
+    }
+
+    document.getElementById('tab-' + name).className += " active";
+    document.getElementById('nav-button-' + name).className += " active";
+}
